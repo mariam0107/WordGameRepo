e5960fbbad09c1264ac21d0bbdac960853a1c33a
mariam george <mariam0107@gmail.com>
2025-02-09 15:19:28 +0100

    First version

---
+++ src/com/mariam/wordgame/WordGuessingGame.java
@@ -0,0 +1,26 @@
+package com.mariam.wordgame;
+
+public class WordGuessingGame {
+    private String wordToGuess;
+    private char[] guessedLetters;
+    private int attempts;
+    public WordGuessingGame(String[] wordList,int maxAttempts){
+      Random rand = new Random();
+      this.wordToGuess = wordList[rand.nextInt(wordList.length)];
+      this.guessedLetters = new char[this.wordToGuess.length()];
+      for (int i = 0; i < this.guessedLetters.length ;i++){
+          this.guessedLetters[i] = '_';
+      }
+      this.attempts = maxAttempts;
+    }
+
+    public  void play(){
+        Scanner scanner = new Scanner(System.in);
+
+        while (attempts > 0){
+            System.out.println("You have"+ attempts +"attempts left...");
+            System.out.println("Guess a letter : ");
+            attempts--;
+        }
+    }
+}
